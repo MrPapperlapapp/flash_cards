cf42a3140b08f5665dc3f80a2bbec5cec27df693
Mannomann <krivmann00@gmail.com>
2023-09-17 13:42:54 +0200

    story book added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import * as ReactDOM from 'react-dom/client'
 
 import { App } from '@/App'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />

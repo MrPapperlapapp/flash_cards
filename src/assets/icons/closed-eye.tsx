f230141dc282ee5efda1345265c87198dc5d0074
ValeriaBog <valeriabog@yandex.ru>
2023-09-20 12:40:12 +0300

    Added text field component

---
+++ src/assets/icons/closed-eye.tsx
@@ -0,0 +1,28 @@
+import { SVGProps, Ref, forwardRef, memo } from 'react'
+
+import s from '../../components/ui/text-field/text-field.module.scss'
+
+const ClosedEye = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    fill="none"
+    viewBox="0 0 24 24"
+    ref={ref}
+    {...props}
+  >
+    <path
+      stroke="#FFF"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      className={s.eye}
+      d="m2.999 3 18 18M9.843 9.914a3 3 0 0 0 4.265 4.22M6.5 6.646A10.024 10.024 0 0 0 2.457 12c1.274 4.057 5.065 7 9.542 7 1.99 0 3.842-.58 5.4-1.582m-6.4-12.369c.329-.032.663-.049 1-.049 4.478 0 8.268 2.943 9.542 7a9.954 9.954 0 0 1-1.189 2.5"
+    />
+  </svg>
+)
+const ForwardRef = forwardRef(ClosedEye)
+const Memo = memo(ForwardRef)
+
+export { Memo as ClosedEye }
---
+++ src/components/ui/text-field/text-field.tsx
@@ -0,0 +1,79 @@
+import { ComponentPropsWithoutRef, forwardRef, useState } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './text-field.module.scss'
+
+import { Close } from '@/assets/icons/close.tsx'
+import { ClosedEye } from '@/assets/icons/closed-eye.tsx'
+import { OpenedEye } from '@/assets/icons/opened-eye.tsx'
+import { Search } from '@/assets/icons/search.tsx'
+import { Typography } from '@/components/ui/typography'
+
+export type TextFieldProps = {
+  errorMessage?: string
+  label?: string
+  clearField?: () => void
+  onValueChange?: (value: string) => void
+} & ComponentPropsWithoutRef<'input'>
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ className, errorMessage, type, label, clearField, onValueChange, ...rest }, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const passwordType = type === 'password'
+    const searchType = type === 'search'
+    const displayClearButton = searchType && clearField && rest.value
+    const classes = {
+      root: clsx(s.root, className),
+      label: clsx(s.label, rest.disabled && s.disabled),
+      input: clsx(s.input, searchType && s.search, rest.value && s.filled, errorMessage && s.error),
+      buttonCloseIcon: clsx(s.button, s.iconClose),
+    }
+    const searchIcon = `${s.searchIcon} ${rest.disabled && s.disabledIcon} ${
+      rest.value && s.filledIcon
+    }`
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      onValueChange?.(e.target.value)
+    }
+
+    return (
+      <div className={classes.root}>
+        <Typography as={'label'} variant={'body2'} className={classes.label}>
+          {label}{' '}
+          <div className={s.textFieldContainer}>
+            <input
+              className={classes.input}
+              ref={ref}
+              type={passwordType && showPassword ? 'text' : type}
+              onChange={handleChange}
+              {...rest}
+            />
+            {passwordType && (
+              <button
+                className={s.button}
+                type={'button'}
+                onClick={() => {
+                  setShowPassword(e => !e)
+                }}
+                disabled={rest.disabled}
+              >
+                {showPassword ? <OpenedEye /> : <ClosedEye />}
+              </button>
+            )}
+            {searchType && <Search className={searchIcon} />}
+            {displayClearButton && (
+              <button type={'button'} onClick={clearField} className={classes.buttonCloseIcon}>
+                <Close />
+              </button>
+            )}
+          </div>
+        </Typography>
+        {errorMessage && (
+          <Typography variant={'caption'} className={s.errorMessage}>
+            {errorMessage}{' '}
+          </Typography>
+        )}
+      </div>
+    )
+  }
+)

2023df43b8cc3857639ceffc7adc32a7398b0c3f
Papperla <krivman@Savar.local>
2023-09-26 14:37:08 +0200

    profile added

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,42 @@
+import { Button, Card, Typography } from '@/components'
+import { Avatar } from '@/components/ui/avatar'
+
+import s from './profile.module.scss'
+import { Edit } from '@/assets/icons/drop-down/edit.tsx'
+
+export const Profile = ({ name, avatarImg, title, email }: ProfileProps) => {
+  return (
+    <Card className={s.profileContainer}>
+      <Typography variant={'large'} mb={27}>
+        {title}
+      </Typography>
+      <div className={s.avatarContainer}>
+        <Avatar userName={'Hallo'} size={96} image={avatarImg} className={s.avatar} />
+        <Button variant={'secondary'} className={s.editAvatarButton}>
+          <Edit className={s.editAvatarIcon} />
+        </Button>
+      </div>
+      <div className={s.nickNameContainer}>
+        <Typography variant={'h1'} mr={3}>
+          {name}
+        </Typography>
+        <Button variant={'primary'} className={s.editNickNameButton}>
+          <Edit className={s.editNickNameIcon} />
+        </Button>
+      </div>
+      <Typography variant={'body2'} color={'var(--color-dark-300)'}>
+        {email}
+      </Typography>
+      <Button variant={'primaryWithIcon'} className={s.logOutButton}>
+        LogOut
+      </Button>
+    </Card>
+  )
+}
+
+type ProfileProps = {
+  title: string
+  avatarImg: string
+  name: string
+  email: string
+}

fc6898d56f5f2fe5346c708dbaf3c8c41cbe5cc3
Nollldor <artur.ishmiev@gmail.com>
2023-10-26 17:11:23 +0300

    feat: create-new-password error handling

--- src/components/auth/create-new-password/create-new-password.tsx
+++ src/components/auth/create-new-password/create-new-password.tsx
@@ -1,56 +1,64 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { Navigate, useParams } from 'react-router-dom'
-import { z } from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+import {useForm} from 'react-hook-form'
+import {Navigate, useParams} from 'react-router-dom'
+import {z} from 'zod'
 
 import s from './create-new-password.module.scss'
-import { createNewPasswordSchema } from './schema.ts'
+import {createNewPasswordSchema} from './schema.ts'
 
-import { Button, Card, ControlledTextField, Typography } from '@/components'
-import { useResetPasswordMutation } from '@/features/auth/model/services/auth.ts'
+import {Button, Card, ControlledTextField, Typography} from '@/components'
+import {useResetPasswordMutation} from '@/features/auth/model/services/auth.ts'
+import {toast} from "react-toastify";
 
 type Form = z.infer<typeof createNewPasswordSchema>
 
 const defaultValues: Form = {
-  newPassword: '',
+    newPassword: '',
 }
 
 export const CreateNewPassword = () => {
-  const { token } = useParams()
+    const {token} = useParams()
 
-  const [resPass, { isSuccess }] = useResetPasswordMutation()
-  const { control, handleSubmit, reset } = useForm<Form>({
-    resolver: zodResolver(createNewPasswordSchema),
-    mode: 'onSubmit',
-    defaultValues,
-  })
-  const onSubmitForm = handleSubmit(data => {
-    token && data.newPassword && resPass({ token, password: data.newPassword })
-    reset(defaultValues)
-  })
+    const [resPass, {isSuccess}] = useResetPasswordMutation()
+    const {control, handleSubmit, reset} = useForm<Form>({
+        resolver: zodResolver(createNewPasswordSchema),
+        mode: 'onSubmit',
+        defaultValues,
+    })
+    const onSubmitForm = handleSubmit(data => {
+        token && data.newPassword && resPass({token, password: data.newPassword})
+            .unwrap()
+            .catch(e => {
+                e.status === 'FETCH_ERROR'
+                    ? toast.error('No internet connection')
+                    : toast.error(e.data.message)
+            })
 
-  if (isSuccess) return <Navigate to={'/login'} />
+        reset(defaultValues)
+    })
 
-  return (
-    <Card className={s.card}>
-      <Typography variant={'large'} as={'h1'} className={s.title}>
-        Create new password
-      </Typography>
-      <form onSubmit={onSubmitForm}>
-        <ControlledTextField
-          control={control}
-          title={'Password'}
-          type={'password'}
-          className={s.password}
-          name={'newPassword'}
-        />
-        <Typography variant={'body2'} color={'secondary'}>
-          Create new password and we will send you further instructions to email
-        </Typography>
-        <Button className={s.button} fullWidth type={'submit'}>
-          Create new password
-        </Button>
-      </form>
-    </Card>
-  )
+    if (isSuccess) return <Navigate to={'/login'}/>
+
+    return (
+        <Card className={s.card}>
+            <Typography variant={'large'} as={'h1'} className={s.title}>
+                Create new password
+            </Typography>
+            <form onSubmit={onSubmitForm}>
+                <ControlledTextField
+                    control={control}
+                    title={'Password'}
+                    type={'password'}
+                    className={s.password}
+                    name={'newPassword'}
+                />
+                <Typography variant={'body2'} color={'secondary'}>
+                    Create new password and we will send you further instructions to email
+                </Typography>
+                <Button className={s.button} fullWidth type={'submit'}>
+                    Create new password
+                </Button>
+            </form>
+        </Card>
+    )
 }
